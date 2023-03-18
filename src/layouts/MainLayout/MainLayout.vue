27d752d257dd7b4e1c6395a8ae3a2b9375d5b24a
Apollo314 <abidinsefa@outlook.com>
2023-03-19 02:36:32 +0300

    lint cleanup and restructure some pages

---
+++ src/layouts/MainLayout/MainLayout.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+    <q-header reveal class="transparent-blur">
+      <q-toolbar>
+        <q-btn
+          flat
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+        <q-toolbar-title>
+          <RouterLink :to="{ name: 'home' }">{{ $t('appName') }}</RouterLink>
+        </q-toolbar-title>
+        <q-toggle
+          :model-value="$q.dark.isActive"
+          @update:model-value="$q.dark.set($event)"
+          checked-icon="dark_mode"
+          color="grey-8"
+          unchecked-icon="light_mode"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <LeftDrawer v-model="leftDrawerOpen"></LeftDrawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import LeftDrawer from './LeftDrawer.vue';
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
+
+<style lang="scss">
+.transparent-blur {
+  background: transparent;
+  backdrop-filter: blur(10px);
+}
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,15 +3,21 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('src/layouts/MainLayout/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/Index/IndexPage.vue'),
+        name: 'home',
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    component: () => import('src/pages/NotFound/ErrorNotFound.vue'),
   },
 ];
 
