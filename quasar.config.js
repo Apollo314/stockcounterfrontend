4ceefc70f11d11432034194605c0c30669f7a5f7
Apollo314 <abidinsefa@outlook.com>
2023-03-22 02:54:32 +0300

    config: quasar plugins and vscode settings

--- quasar.config.js
+++ quasar.config.js
@@ -64,7 +64,7 @@ module.exports = configure(function (/* ctx */) {
 
       // publicPath: '/',
       // analyze: true,
-      // env: {},
+      env: require('dotenv').config().parsed,
       // rawDefine: {}
       // ignorePublicFolder: true,
       // minify: false,
@@ -114,7 +114,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ['Notify', 'Dark', 'Dialog', 'Loading', 'Screen'],
     },
 
     // animations: 'all', // --- includes all animations
