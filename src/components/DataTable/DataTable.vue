f9424b63a8349cd2d9af04441d099fc7d697ee6e
Apollo314 <abidinsefa@outlook.com>
2023-03-24 01:07:20 +0300

    table styling and stuff

--- src/components/DataTable/DataTable.vue
+++ src/components/DataTable/DataTable.vue
@@ -1,59 +1,70 @@
 <template>
-  <q-markup-table separator="cell" flat class="coolshadow" bordered>
-    <thead class="bg-teal-8 text-white">
-      <tr>
-        <th style="width: 0px" @click.stop="toggleSelectionAll()">
-          <q-checkbox
-            dense
-            :model-value="selectAllButtonStatus"
-            @click="toggleSelectionAll()"
-          />
-        </th>
-        <th
-          v-for="column in computedcolumns"
-          :key="column.id"
-          :class="getAlignClass(column.align)"
+  <adaptive-card
+    :class="{
+      'sticky-header': stickyHeader,
+      'sticky-caption': stickyCaption,
+    }"
+    no-width-scrollbar
+    :compact-scrollbar="false"
+  >
+    <table flat class="fit coolshadow data-table">
+      <caption class="table-caption" :class="captionClasses">
+        <slot name="caption" v-bind="{ selected, pagination }"></slot>
+      </caption>
+      <thead class="table-head text-white" :class="theadClasses">
+        <tr>
+          <th class="table-header-cell" @click.stop="toggleSelectionAll()">
+            <q-checkbox
+              dense
+              class="checkbox"
+              :model-value="selectAllButtonStatus"
+              @click="toggleSelectionAll()"
+            />
+          </th>
+          <th
+            class="table-header-cell"
+            v-for="column in computedcolumns"
+            :key="column.id"
+            :class="getAlignClass(column.align)"
+          >
+            <b>
+              {{ column.label }}
+            </b>
+            <slot :name="`th-inner-sibling-${column.id}`" v-bind="{ column }" />
+          </th>
+          <slot name="head-tr-inner" />
+        </tr>
+      </thead>
+      <tbody class="table-body" :class="tbodyClasses">
+        <tr
+          v-for="row in data"
+          :class="{ 'selected-row': selected.indexOf(row) > -1 }"
+          :key="row.id"
         >
-          <b>
-            {{ column.label }}
-          </b>
-          <slot :name="`th-inner-sibling-${column.id}`" v-bind="{ column }" />
-        </th>
-        <slot name="head-tr-inner" />
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="row in data"
-        :class="{ 'selected-row': selected.indexOf(row) > -1 }"
-        :key="row.id"
-      >
-        <td
-          style="width: 0px"
-          @click.stop="toggleSelection(row, !(selected.indexOf(row) > -1))"
-        >
-          <q-checkbox
-            dense
-            :model-value="selected.indexOf(row) > -1"
-            @update:model-value="toggleSelection(row, $event)"
-          />
-        </td>
-        <td
-          :class="getAlignClass(column.align)"
-          v-for="column in computedcolumns"
-          :key="column.id"
-        >
-          {{ column.field(row) }}
-          <slot
-            :name="`td-inner-sibling-${column.id}`"
-            v-bind="{ row: row, column: column }"
-          />
-        </td>
-        <slot name="body-tr-inner" v-bind="{ row }" />
-      </tr>
-    </tbody>
-  </q-markup-table>
-  <pre>{{ selected }}</pre>
+          <td @click.stop="toggleSelection(row, !(selected.indexOf(row) > -1))">
+            <q-checkbox
+              dense
+              class="checkbox"
+              :model-value="selected.indexOf(row) > -1"
+              @update:model-value="toggleSelection(row, $event)"
+            />
+          </td>
+          <td
+            :class="getAlignClass(column.align)"
+            v-for="column in computedcolumns"
+            :key="column.id"
+          >
+            {{ column.field(row) }}
+            <slot
+              :name="`td-inner-sibling-${column.id}`"
+              v-bind="{ row: row, column: column }"
+            />
+          </td>
+          <slot name="body-tr-inner" v-bind="{ row }" />
+        </tr>
+      </tbody>
+    </table>
+  </adaptive-card>
 </template>
 
 <script
@@ -61,8 +72,9 @@
   lang="ts"
   generic="Row extends BaseRow, Column extends BaseColumn<Row>, Filters extends Object"
 >
-import { QMarkupTable } from 'quasar';
 import { computed, ref, Ref } from 'vue';
+
+import AdaptiveCard from '../Card/AdaptiveCard.vue';
 
 export type BaseRow = {
   id: number | string;
@@ -83,11 +95,21 @@ export type Pagination<Filters> = {
   filters: Filters;
 };
 
-const props = defineProps<{
-  columns: Column[] | (() => Column[]);
-  data: Row[];
-  pagination?: Pagination<Filters>;
-}>();
+const props = withDefaults(
+  defineProps<{
+    columns: Column[] | (() => Column[]);
+    data: Row[];
+    pagination?: Pagination<Filters>;
+    stickyCaption?: boolean;
+    stickyHeader?: boolean;
+    captionClasses?: string;
+    theadClasses?: string;
+    tbodyClasses?: string;
+  }>(),
+  {
+    stickyHeader: true,
+  }
+);
 
 const selected: Ref<Row[]> = ref([]);
 
@@ -107,7 +129,7 @@ function toggleSelection(row: Row, value: boolean) {
 function toggleSelectionAll() {
   // if completely unselected, select all
   if (selected.value.length === 0) {
-    selected.value = props.data;
+    selected.value = [...props.data];
   }
   // if any selected, deselect
   else if (selected.value.length > 0) {
@@ -142,8 +164,115 @@ const computedcolumns = computed(() => {
 });
 </script>
 
-<style scoped lang="scss">
-tr.selected-row {
-  background: rgba($primary, 0.2);
+<style lang="scss">
+$cellpadding: 12px;
+$captionheight: 50px;
+
+$secondcolor-dark: #162429;
+
+$secondcolor-light: #e1eff4;
+
+$separator-color: rgba(0, 0, 0, 0.14) !default;
+$separator-dark-color: rgba(255, 255, 255, 0.14) !default;
+
+// table
+$table-light-background: $almost-white; // custom
+$table-border-color: $separator-color !default;
+$table-hover-background: hsla(197, 60%, 50%, 0.3);
+$table-light-selected-background: lighten($warning, 20%);
+
+$table-dark-border-color: $separator-dark-color !default;
+$table-dark-hover-background: hsla(197, 60%, 50%, 0.3);
+$table-dark-selected-background: darken($warning, 40%);
+
+.data-table {
+  // border-radius: $generic-border-radius;
+  border-collapse: collapse;
+  position: relative;
+  .checkbox {
+    padding: 4px;
+  }
+  thead.table-head {
+    th.table-header-cell {
+      background-color: $teal-8;
+      border-collapse: collapse;
+      border: none;
+      .sticky-header & {
+        position: sticky;
+        top: 0;
+        z-index: 100;
+      }
+      .sticky-caption & {
+        top: $captionheight;
+      }
+    }
+  }
+  th,
+  td {
+    padding: $cellpadding;
+    .body--dark & {
+      border: 1px solid $separator-dark-color;
+    }
+    .body--light & {
+      border: 1px solid $separator-color;
+    }
+    &:first-of-type {
+      width: 1px;
+      border-left: none;
+    }
+    &:last-of-type {
+      border-right: none;
+    }
+  }
+
+  .body--dark & {
+    tr:nth-of-type(2n) {
+      background: $secondcolor-dark;
+    }
+    tr.selected-row {
+      position: relative;
+      background: $table-dark-selected-background;
+      // &::after {
+      //   content: '';
+      //   position: absolute;
+      //   left: 0;
+      //   top: 0;
+      //   height: 100%;
+      //   width: 100%;
+      //   background: $table-dark-selected-background;
+      //   pointer-events: none;
+      // }
+      // background: rgba($accent, 0.2);
+    }
+  }
+
+  .body--light & {
+    tr:nth-of-type(2n) {
+      background: $secondcolor-light;
+    }
+    tr.selected-row {
+      position: relative;
+      background: $table-light-selected-background;
+    }
+  }
+
+  tr:last-of-type td {
+    border-bottom: none;
+  }
+
+  .table-caption {
+    background: $secondary;
+    height: $captionheight;
+    .sticky-caption & {
+      position: sticky;
+      top: 0;
+      z-index: 101;
+    }
+  }
+
+  .table-body {
+    height: 100%;
+    overflow: auto;
+  }
 }
 </style>
