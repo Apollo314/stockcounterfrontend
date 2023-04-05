d5d8367c29a5aa7bec526f5c2e69bf88a9312d01
Apollo314 <abidinsefa@outlook.com>
2023-04-06 00:21:35 +0300

    feat: component map for filter component

--- src/components/DataTable/TableFilter.vue
+++ src/components/DataTable/TableFilter.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-    <pre v-for="(component, key) in formComponents" :key="key">
-    {{ key }}
-    {{ component }}</pre
+  <form @reset="resetForm()">
+    <q-btn
+      color="negative"
+      icon="delete"
+      :label="$t('commons.reset')"
+      type="reset"
+    />
+    <div
+      class="full-height q-py-xs"
+      v-for="(formComponent, key) in formComponents"
+      :key="key"
     >
-  </div>
+      <component
+        v-if="keyIsntForPagination(key)"
+        :is="getComponent(formComponent).component"
+        v-bind="{
+          ...getComponent(formComponent).props,
+          ...formComponent.props,
+        }"
+        :name="key"
+      ></component>
+    </div>
+  </form>
 </template>
 
 <script setup lang="ts">
-import { PropType } from 'vue';
+import { useForm } from 'vee-validate';
+import { PropType, toRef } from 'vue';
 
+import { componentMap } from 'components/VeeDynamicForm/componentMap';
 import { FormComponent } from 'src/composables/openapihelpers';
 
 const props = defineProps({
@@ -17,9 +36,34 @@ const props = defineProps({
     type: Object as PropType<Record<string, FormComponent>>,
     required: true,
   },
+  initialFilters: {
+    type: Object as PropType<Record<string, unknown>>,
+    required: false,
+  },
 });
 
-console.log('formcomps', props.formComponents);
+const { values, resetForm, handleSubmit, setValues } = useForm({
+  initialValues: toRef(props, 'initialFilters', {}),
+});
+
+const getComponent = (formComponent: FormComponent) => {
+  let componentDefinition = componentMap[formComponent.componentString];
+  if (componentDefinition === undefined) {
+    componentDefinition = componentMap['text-input'];
+    componentDefinition.props = {
+      label: 'Define me, I am undefined',
+      hint: formComponent.componentString,
+    };
+  }
+  return componentDefinition;
+};
+
+const keyIsntForPagination = (key: string) => {
+  if (['limit', 'offset', 'ordering', 'search'].includes(key)) {
+    return false;
+  }
+  return true;
+};
 </script>
 
 <style scoped></style>
---
+++ src/components/VeeDynamicForm/componentMap.ts
@@ -0,0 +1,137 @@
+import { api } from 'src/boot/axios';
+
+import { $t } from '../../boot/i18n';
+import { CancelablePromise } from '../../client';
+
+import CheckBox from './CustomComponents/CheckBox.vue';
+import DateRange from './CustomComponents/DateRange.vue';
+import DefaultField from './CustomComponents/DefaultField.vue';
+import EnumSelector from './CustomComponents/EnumSelector.vue';
+import NumberInput from './CustomComponents/NumberInput.vue';
+import NumberRange from './CustomComponents/NumberRange.vue';
+import SearchSelector from './CustomComponents/SearchSelector.vue';
+// import BarcodeScanner from './CustomComponents/BarcodeScanner.vue';
+// import ImageSelector from './CustomComponents/ImageSelector.vue';
+
+import type { Component } from 'vue';
+
+export const componentStrings = [
+  'text-input',
+  'category-selector',
+  'multi-category-selector',
+  'unit-selector',
+  'user-select',
+  'item-select',
+  'enum-selector',
+  'checkbox',
+  // 'single-image-selector',
+  'money-input',
+  'money-range',
+  // 'barcode-scanner',
+  'date-time-range',
+] as const;
+
+export type ComponentStrings = (typeof componentStrings)[number];
+
+type Query = {
+  limit?: number;
+  offset?: number;
+  ordering?: string;
+  search?: string;
+};
+
+export type PaginatedResult = {
+  count?: number;
+  next?: string | null;
+  previous?: string | null;
+  results?: Array<unknown>;
+};
+
+export type QueryService = (query: Query) => CancelablePromise<PaginatedResult>;
+
+export function queryServiceFactory<
+  S extends Exclude<keyof typeof api, 'request'>,
+  O extends Extract<keyof (typeof api)[S], `${string}List`>
+>(service: S, operation: O) {
+  return (query: Query) => {
+    return (api[service][operation] as QueryService)(query);
+  };
+}
+
+type ComponentMap = {
+  [key in ComponentStrings]: {
+    /**
+     * A vue component
+     */
+    component: Component;
+    props?:
+      | Record<string, unknown>
+      | {
+          queryService: ReturnType<typeof queryServiceFactory>;
+          optionLabel: (option: Record<string, unknown>) => string;
+        };
+  };
+};
+
+export const componentMap: ComponentMap = {
+  'text-input': { component: DefaultField },
+  'category-selector': {
+    component: SearchSelector,
+    props: {
+      queryService: queryServiceFactory('inventory', 'inventoryCategoryList'),
+      optionLabel: (option) => option?.name as string,
+    },
+  },
+  'multi-category-selector': {
+    component: SearchSelector,
+    props: {
+      queryService: queryServiceFactory('inventory', 'inventoryCategoryList'),
+      optionLabel: (option) => option?.name as string,
+      multiple: true,
+      useChips: true,
+    },
+  },
+  'unit-selector': {
+    component: SearchSelector,
+    props: {
+      queryService: queryServiceFactory('inventory', 'inventoryStockUnitList'),
+      optionLabel: (option) => option?.name as string,
+    },
+  },
+  'user-select': {
+    component: SearchSelector,
+    props: {
+      queryService: queryServiceFactory('user', 'userAccountsList'),
+      optionLabel: (option) => option?.username as string,
+    },
+  },
+  'item-select': {
+    component: SearchSelector,
+    props: {
+      queryService: queryServiceFactory('inventory', 'inventoryItemList'),
+      optionLabel: (option) => option?.name as string,
+    },
+  },
+  'enum-selector': {
+    component: EnumSelector,
+  },
+  checkbox: {
+    component: CheckBox,
+  },
+  'money-range': {
+    component: NumberRange,
+    props: {
+      leftLabel: $t('labels.minimum'),
+      rightLabel: $t('labels.maximum'),
+      title: $t('labels.money-range'),
+    },
+  },
+  'date-time-range': {
+    component: DateRange,
+    props: {},
+  },
+  'money-input': {
+    component: NumberInput,
+    props: {},
+  },
+};
