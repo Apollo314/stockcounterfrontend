7e87d69ac7c7242a5c1bf98fe1dcbffb75474730
Apollo314 <abidinsefa@outlook.com>
2023-04-05 22:32:33 +0300

    feat: default field

---
+++ src/components/VeeDynamicForm/CustomComponents/DefaultField.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <q-input
+      :standout="standout"
+      :outlined="outlined"
+      v-model="value"
+      :type="type"
+      :label="label"
+      @blur="validate()"
+      :clearable="clearable"
+      :error="!!errorMessage"
+      :error-message="errorMessage"
+      clear-icon="clear"
+      :dense="dense"
+      autocomplete="new-password"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useField } from 'vee-validate';
+import { toRef } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    name: string;
+    label: string;
+    standout?: string | boolean;
+    outlined?: boolean;
+    clearable?: boolean;
+    dense?: boolean;
+    type?:
+      | 'number'
+      | 'text'
+      | 'search'
+      | 'password'
+      | 'textarea'
+      | 'email'
+      | 'tel'
+      | 'file'
+      | 'url'
+      | 'time'
+      | 'date';
+  }>(),
+  {
+    type: 'text',
+    standout: true,
+    clearable: true,
+  }
+);
+
+const { value, validate, errorMessage } = useField<string>(
+  toRef(props, 'name')
+);
+</script>
+
+<style scoped></style>
