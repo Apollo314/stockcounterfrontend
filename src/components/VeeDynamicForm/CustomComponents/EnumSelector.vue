055cfa85bff2ce8d9af860d5afd55497b57dc710
Apollo314 <abidinsefa@outlook.com>
2023-04-05 22:32:50 +0300

    feat: enum field

---
+++ src/components/VeeDynamicForm/CustomComponents/EnumSelector.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <q-select
+      :standout="standout"
+      tabindex="0"
+      behavior="menu"
+      class="label-top nice-select"
+      :popup-content-class="
+        ['nice-select-popup', 'coolshadow', 'compact-scrollbar'].join(' ')
+      "
+      options-selected-class="bg-teal text-white"
+      v-model="value"
+      :options="options"
+      :label="label"
+      :label-color="labelColor"
+      :error="!!errorMessage"
+      :error-message="errorMessage"
+      @input-value="validate()"
+      @blur="validate()"
+      :clearable="clearable"
+      clear-icon="clear"
+      :dense="dense"
+    >
+    </q-select>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useField } from 'vee-validate';
+import { toRef, onMounted } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    options: string[] | { label: string; value: unknown }[];
+    nullable?: boolean;
+    default?: unknown;
+    label?: string;
+    labelColor?: string;
+    name: string;
+    standout?: string | boolean;
+    clearable?: boolean;
+    dense?: boolean;
+  }>(),
+  { standout: true }
+);
+
+const { value, errorMessage, validate } = useField(toRef(props, 'name'));
+
+onMounted(() => {
+  if (props.default) {
+    value.value = props.default;
+  }
+});
+</script>
+
+<style scoped></style>
