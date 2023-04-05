fd5b1f2e309cb2aebfd96fc8c8d5ced04c47d259
Apollo314 <abidinsefa@outlook.com>
2023-04-05 08:09:22 +0300

    feat: Datatable filter drawer

---
+++ src/components/DataTable/TableFilter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <pre v-for="(component, key) in formComponents" :key="key">
+    {{ key }}
+    {{ component }}</pre
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+import { FormComponent } from 'src/composables/openapihelpers';
+
+const props = defineProps({
+  formComponents: {
+    type: Object as PropType<Record<string, FormComponent>>,
+    required: true,
+  },
+});
+
+console.log('formcomps', props.formComponents);
+</script>
+
+<style scoped></style>
