e9d39e7d28c0bd1e404a6e4586c1f2e68dc8265c
Apollo314 <abidinsefa@outlook.com>
2023-04-13 00:40:25 +0300

    undefined component as warning

---
+++ src/components/VeeDynamicForm/CustomComponents/UndefinedWarningComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="bg-negative">I am undefined, please, define me.</div>
+    <div class="bg-negative">{{ props }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  name: string;
+}>();
+</script>
+
+<style scoped></style>
