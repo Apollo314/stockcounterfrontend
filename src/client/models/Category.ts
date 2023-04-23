b76d3e73c462a75ee2003be9b00c0cc275c7d7f3
Apollo314 <abidinsefa@outlook.com>
2023-04-23 18:51:12 +0300

    category pages

--- src/client/models/Category.ts
+++ src/client/models/Category.ts
@@ -2,8 +2,11 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { SimpleCategory } from './SimpleCategory';
+
 export type Category = {
   readonly id: number;
   name: string;
   parent?: number | null;
+  readonly children: Array<SimpleCategory>;
 };
